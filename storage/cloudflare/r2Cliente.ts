d9788458b7a3a3e86b0e19af1dc14fda1c331d48
bozomj <bozomj@gmail.com>
2026-09-16 00:03:35 -0300

    add flag  forcePathStyle: true,in r2

--- storage/cloudflare/r2Cliente.ts
+++ storage/cloudflare/r2Cliente.ts
@@ -10,6 +10,7 @@ import fs from "fs";
 export const r2Client = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY!,
     secretAccessKey: process.env.CLOUDFLARE_ACCESS_SECRET_KEY!,
